94158bd27bd71d3b8255b25d9cd1483a9288af4e
Matthew Upson <ivyleavedtoadflax@gmail.com>
2015-12-27 08:08:09 +0000

    added order by

--- postgres/daily_elec.sql
+++ postgres/daily_elec.sql
@@ -1,5 +1,5 @@
 create view daily_elec as
-	 select date_trunc('day', to_timestamp(timestamp,'YYYY-MM-DD HH24:MI:SS')) as
+	 select date_trunc('day', to_timestamp(timestamp,'YYYY-MM-DD HH24:MI:SS'))::date as
 	 "day", round(sum(value)::numeric,2) as "sum", count(*) as "n"
-	 from elec group by 1;
+	 from elec group by 1 order by day;
 
--- postgres/daily_elec_adu.sql
+++ postgres/daily_elec_adu.sql
@@ -1,5 +1,5 @@
 create view daily_elec_adu as
 	 select date_trunc('day', to_timestamp(timestamp,'YYYY-MM-DD HH24:MI:SS'))::date as
 	 "day", round(sum(value)::numeric,2) as "sum", count(*) as "n"
-	 from elec_adu group by 1;
+	 from elec_adu group by 1 order by day;
 
